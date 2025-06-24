cfb067d84283c3b0796f504e6f859438673c427b
Ethan <415561402@qq.com>
2025-06-24 20:58:31 +0800

    🚀 chore: init

---
+++ eslint.config.ts
@@ -0,0 +1,7 @@
+import defineConfig from "@xiaohe01/eslint-config";
+
+export default defineConfig({
+  pnpm: true,
+  type: "lib",
+  markdown: false
+});
---
+++ packages/core/src/components/uni-tree-list.vue
@@ -0,0 +1,9 @@
+<template>
+  <view>111111</view>
+</template>
+
+<script lang="ts" setup>
+
+</script>
+
+<style lang="scss" scoped></style>
