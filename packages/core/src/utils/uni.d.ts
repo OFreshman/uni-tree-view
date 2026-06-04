e1282a70fe00dab6b7f2a6abd4b568456a24638a
Kieran <33643257+OFreshman@users.noreply.github.com>
2026-06-04 23:27:06 +0800

    （vbc）修复核心包构建链路

--- packages/core/src/utils/uni.d.ts
+++ packages/core/src/utils/uni.d.ts
@@ -1,4 +1,4 @@
-import type { VueThis } from "../composables/useVueThis";
+import type { ComponentPublicInstance } from "vue";
 
 export declare function getDeviceInfo(): UniApp.GetDeviceInfoResult | UniApp.GetSystemInfoResult;
 
@@ -11,7 +11,7 @@ export declare function getVersion(): string;
 export declare function compareVersion(v1: string, v2: string): 0 | 1 | -1;
 
 export declare function querySelect(
-  component: VueThis,
+  component: ComponentPublicInstance,
   selector: string,
   fields: UniApp.NodeField
 ): Promise<UniApp.NodeInfo>;
