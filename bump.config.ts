6a39dac4a115b5df34ef5bd2acc1d51ba25a24ca
Kieran <415561402@qq.com>
2026-07-29 17:37:27 +0800

    chore: 精简版本发布输出

--- bump.config.ts
+++ bump.config.ts
@@ -4,6 +4,7 @@ export default defineConfig({
   all: true,
   commit: "chore: release v%s",
   execute: "pnpm release:prepare",
+  printCommits: false,
   push: false,
   recursive: true,
   tag: "v%s"
